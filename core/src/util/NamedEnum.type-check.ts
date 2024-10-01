41f0965e064171b125eb3d3dcfe341e4c6eba49f
Colin <biz@colinhan.info>
2024-10-01 13:20:04 +0800

    Ensure type validation.

--- core/src/util/NamedEnum.type-check.ts
+++ core/src/util/NamedEnum.type-check.ts
@@ -4,7 +4,7 @@ import NamedEnum, { aliasableSymbol } from "./NamedEnum";
 const { check, checks } = Test;
 
 interface MyEnum {
-  hours: number;
+    hours: number;
 }
 
 const MyEnumWeek: MyEnum = { hours: 8 * 5 };
@@ -12,48 +12,51 @@ const MyEnumDay: MyEnum = { hours: 8 };
 const MyEnumHour: MyEnum = { hours: 1 };
 
 export const MyEnums = NamedEnum({
-  WEEK: { alias: ['week', 'weeks', 'w'], value: MyEnumWeek },
-  DAY: { alias: ['day', 'days', 'd'], value: MyEnumDay },
-  HOUR: { alias: ['hour', 'hours', 'h'], value: MyEnumHour }
+    WEEK: { alias: ['week', 'weeks', 'w'], value: MyEnumWeek },
+    DAY: { alias: ['day', 'days', 'd'], value: MyEnumDay },
+    HOUR: { alias: ['hour', 'hours', 'h'], value: MyEnumHour }
 });
 
 export type MyEnumsComp = A.Compute<{
-  WEEK: MyEnum;
-  DAY: MyEnum;
-  HOUR: MyEnum;
-  parse: (text: string) => MyEnum | undefined;
+    WEEK: MyEnum;
+    DAY: MyEnum;
+    HOUR: MyEnum;
+    parse: (text: string) => MyEnum | undefined;
 }>
 
 class MyEnum2 {
-  private alias: string[];
+    private alias: string[];
 
-  [aliasableSymbol](): string[] {
-    return this.alias;
-  }
+    [aliasableSymbol](): string[] {
+        return this.alias;
+    }
 
-  constructor(alias: string[]) {
-    this.alias = alias;
-  }
+    constructor(alias: string[]) {
+        this.alias = alias;
+    }
 }
 
 const MyEnum2Week = new MyEnum2(['week', 'weeks', 'w']);
 const MyEnum2Day = new MyEnum2(['day', 'days', 'd']);
 
 export const MyEnum2s = NamedEnum({
-  WEEK: MyEnum2Week,
-  DAY: MyEnum2Day
+    WEEK: MyEnum2Week,
+    DAY: MyEnum2Day
 });
 
 export interface MyEnum2sComp {
-  WEEK: MyEnum2;
-  DAY: MyEnum2;
-  parse: (text: string) => MyEnum2 | undefined;
+    WEEK: MyEnum2;
+    DAY: MyEnum2;
+    parse: (text: string) => MyEnum2 | undefined;
 }
 
 checks([
-  check<typeof MyEnums, MyEnumsComp, Test.Pass>()
+    check<typeof MyEnums, MyEnumsComp, Test.Pass>()
 ]);
 
 checks([
-  check<typeof MyEnum2s, MyEnum2sComp, Test.Pass>()
+    check<typeof MyEnum2s, MyEnum2sComp, Test.Pass>()
 ]);
+
+//@ts-expect-error options can't be empty
+export const MyEmptyEnum = NamedEnum({});
